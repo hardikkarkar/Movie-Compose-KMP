b3f654c947c86b5c6f691eba1e56c58774062b83
Hardik Karkar <hardikkarkar007@gmail.com>
2025-04-30 20:27:21 +0530

    top bar adjusted

--- composeApp/src/commonMain/kotlin/com/hardik/movieapp/cmp/presentation/navigations/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/hardik/movieapp/cmp/presentation/navigations/MainScreen.kt
@@ -3,6 +3,7 @@ package com.hardik.movieapp.cmp.presentation.navigations
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -15,6 +16,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.hardik.movieapp.cmp.isWeb
@@ -48,6 +50,8 @@ fun MainScreen(navController: NavHostController) {
                     if (isWeb())
                         BottomNavigationBar(navController = navController)
                 }
+            } else {
+                Box(modifier = Modifier.height(0.dp))
             }
         },
         bottomBar = {
